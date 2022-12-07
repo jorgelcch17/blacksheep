6af7efecf9ddf9b4bab284da2ceb5b34dcd1eb25
jorgel <jorgelcch17@gmail.com>
2022-12-07 14:34:17 -0400

    admin editar categoria

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Livewire\WishlistComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
 /*
@@ -62,6 +63,7 @@ Route::middleware(['auth', 'authadmin'])->group(function(){
     Route::get('/admin/dashboard', UserDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoriesComponent::class)->name('admin.categories');
     Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.category.add');
+    Route::get('/admin/category/edit/{category_id}', AdminEditCategoryComponent::class)->name('admin.category.edit');
 });
 
 
