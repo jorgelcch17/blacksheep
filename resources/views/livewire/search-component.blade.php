db43c7bfe69373f18e2057dabb82f44966075824
jorgel <jorgelcch17@gmail.com>
2022-12-10 02:49:17 -0400

    admin agregar producto

--- resources/views/livewire/search-component.blade.php
+++ resources/views/livewire/search-component.blade.php
@@ -88,7 +88,7 @@
                                             <div class="product-img product-img-zoom">
                                                 <a href="{{ route('product.details', $product->slug) }}">
                                                     <img class="default-img"
-                                                        src="{{ asset('assets/imgs/shop/product-') }}{{ $product->id }}-1.jpg"
+                                                        src="{{ asset('assets/imgs/products') }}/{{ $product->image }}"
                                                         alt="{{ $product->name }}">
                                                     <img class="hover-img"
                                                         src="{{ asset('assets/imgs/shop/product-') }}{{ $product->id }}-2.jpg"
