4247cef76b8f59ec81ca8bf60bb4186092868581
jorgel <jorgelcch17@gmail.com>
2022-12-17 21:03:32 -0400

    admin mostrar productos destacados en el home

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -5,13 +5,22 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\HomeSlider;
 use App\Models\Product;
+use Cart;
 
 class HomeComponent extends Component
 {
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::instance('cart')->add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+        session()->flash('success_message', 'Producto añadido al carrito');
+        return redirect()->route('shop.cart');
+    }
+
     public function render()
     {
         $slides = HomeSlider::where('status', 1)->get();
         $lproducts = Product::orderBy('created_at', 'DESC')->get(); // Latest Products for Home Page
-        return view('livewire.home-component', compact('slides', 'lproducts'));
+        $fproducts = Product::where('featured', 1)->inRandomOrder()->get()->take(8); // prdoucts featured
+         return view('livewire.home-component', compact('slides', 'lproducts', 'fproducts'));
     }
 }
