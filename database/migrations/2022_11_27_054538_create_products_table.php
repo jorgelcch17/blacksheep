3038a24b2d681cc798df28aac35a208287fa5900
jorgel <jorgelcch17@gmail.com>
2023-01-18 10:58:06 -0400

    arreglo de los nombres de productos en unique

--- database/migrations/2022_11_27_054538_create_products_table.php
+++ database/migrations/2022_11_27_054538_create_products_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('slug');
             $table->string('short_description')->nullable();
             $table->text('description')->nullable();
             $table->decimal('regular_price', 8, 2);
